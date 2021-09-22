fa0aff34221fe9253526ae05749528f17de80072
unknown <sanninm>
2021-09-23 00:23:37 +0200

    Complex Objects

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,31 +1,33 @@
 function myFunction(){
-var myMusic = {
-    001: {
-        artist: "Billy Joel",
-        title: "Piano Man",
-        relase_year: 1073,
-        formats: {
-            1: "CD",
-            2: "8T",
-            3: "LP"
-
+    var myMusic = {
+        001: {
+            artist: "Billy Joel",
+            title: "Piano Man",
+            relase_year: 1073,
+            formats: {
+                1: "CD",
+                2: "8T",
+                3: "LP"
+    
+            },
+            gold: true
         },
-        gold: true
-    },
-    002: {
-        artist: "Billy Joel",
-        title: "Piano Man",
-        relase_year: 1073,
-        formats: {
-            1: "CD",
-            2: "8T",
-            3: "LP"
-
-        },
-        gold: true
+        002: {
+            artist: "Dino Merlin",
+            title: "Hotel Nacional",
+            relase_year: 2014,
+            formats: {
+                1: "CD",
+                2: "YT",
+                3: "LP"
+    
+            },
+            gold: true
+        }
+    };
+    return myMusic;
     }
-};
-}
-console.log(myFunction());
-myFunction()[2];
-module.exports = myFunction;
+    console.log(myFunction());
+    myFunction()[2];
+    module.exports = myFunction;
+    
