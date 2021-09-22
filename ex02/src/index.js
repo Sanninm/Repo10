7c332c748d61b78d8a3f0b5d6173066f1c788d76
unknown <sanninm>
2021-09-22 23:53:36 +0200

    Dot Notation

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -5,9 +5,9 @@ shirt: "Nike",
 shoes: "Converse"
 };
 function myFunction(myClothes) {
-hatValue = myClothes.hat;
-shirtValue = myClothes.shirt;
-shoesValue = myClothes.shoes;
+var hatValue = myClothes.hat;
+var shirtValue = myClothes.shirt;
+var shoesValue = myClothes.shoes;
 return { hatValue, shirtValue, shoesValue };
 }
 // Only change code above this line 
