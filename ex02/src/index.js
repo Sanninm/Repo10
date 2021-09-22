d9a8b5a247b7f16a1f6556bf30715bdf7ea405d6
unknown <sanninm>
2021-09-22 18:18:02 +0200

    Dot Notation

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line 
+var myClothes = {
+hat: "Fedora",
+shirt: "Nike",
+shoes: "Converse"
+};
+function myFunction(myClothes) {
+hatValue = myClothes.hat;
+shirtValue = myClothes.shirt;
+shoesValue = myClothes.shoes;
+return { hatValue, shirtValue, shoesValue };
+}
+// Only change code above this line 
+    console.log(myFunction(myClothes));
+    module.exports = myFunction(myClothes);
