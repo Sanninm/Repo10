e5c502e2a57b17a3c4aaa9227addeea1710e943d
unknown <sanninm>
2021-09-22 18:01:52 +0200

    Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line 
+var myPet = {
+species: "Tiger",
+name: "Porto",
+legs: 4,
+friends: "Cat"
+}
+
+function myFunction(obj) {
+    return obj;
+}
+
+// Only change code above this line 
+console.log(myFunction(myPet));
+module.exports = { myPet, myFunction };
