43142e0767382595f38f0a8de966895ccda5db55
unknown <sanninm>
2021-09-22 20:06:36 +0200

    “Lookups”

---
+++ ex08/src/index.js
@@ -0,0 +1,29 @@
+function getUsername(email) {
+    var username = {
+    // Only change code below this line 
+    "user1@mail.com": "user1",
+    "user2@mail.com": "user2",
+    "user3@mail.com": "user3",
+    "user4@mail.com": "user4",
+    "user5@mail.com": "user5",
+    "user6@mail.com": "user6"  
+}; 
+    var value = username[email]
+    return value;
+}
+// Only change code above this line
+getUsername("user1@mail.com")
+getUsername("user2@mail.com")
+getUsername("user3@mail.com")
+getUsername("user4@mail.com")
+getUsername("user5@mail.com")
+getUsername("user6@mail.com")
+
+console.log(getUsername("user1@mail.com"));
+console.log(getUsername("user2@mail.com"));
+console.log(getUsername("user3@mail.com"));
+console.log(getUsername("user4@mail.com"));
+console.log(getUsername("user5@mail.com"));
+console.log(getUsername("user6@mail.com"));
+console.log(getUsername(""));
+module.eport = getUsername;
